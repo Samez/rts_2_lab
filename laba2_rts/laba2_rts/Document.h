9cf1d53bf1ed5abce861396bdcca970d6a210fe3
Samez <samez666@me.com>
2013-03-30 15:29:32 +0400

    initial

---
+++ laba2_rts/laba2_rts/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  laba2_rts
+//
+//  Created by Samez on 30.03.13.
+//  Copyright (c) 2013 gg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ laba2_rts/laba2_rts/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  laba2_rts
+//
+//  Created by Samez on 30.03.13.
+//  Copyright (c) 2013 gg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
